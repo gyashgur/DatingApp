f779571fbacf01a22b21aac95dc5d3d84d72ce07
gyashgur <gyashgur@icloud.com>
2019-09-20 11:51:22 -0400

    Initial Commit

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Controllers
+{
+    //GET  http://localhost:5000/api/values
+    [Route("api/[controller]")]
+    [ApiController]
+    
+    
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext _context;    // Table
+        public ValuesController(DataContext context)    // Inject data context class
+        {
+            _context = context;
+        }
+
+    // GET api/values
+    [HttpGet]
+        public async Task<IActionResult> GetValues()      // Not only strings in HTTP 
+                                                                   // response Instead of ActionResult<IEnumerable<string>
+        {
+           // was for test throw new Exception("Test Exception");
+
+           // return new string[] { "value1", "value4" };
+           var values = await _context.Values.ToListAsync();                  // DbSet<Value> Values
+           return Ok(values);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync ( x => x.Id == id);
+           return Ok(value);
+    
+            //return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
